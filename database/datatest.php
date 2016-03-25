f7f01fa4f3c7981a18c52c7926a28ca10b69dae4
maxwellstorm <mxs5194@rit.edu>
2016-03-25 18:20:42 -0400

    added more to data layer classes more comming soon

--- database/datatest.php
+++ database/datatest.php
@@ -1,5 +1,6 @@
 <?php
 require("data.php");
+require("department.php");
 
 $testDb = new data; //connects to database in constructor
 
@@ -12,6 +13,15 @@ printExample($res);
 echo "<br/><br/><strong>select with where stmt example:</strong>";
 $res = $testDb->getData("select * from department where departmentId = :depId",array("depId"=>1));
 printExample($res);
+
+
+$testDept = new department($testDb,"1");
+$testDept->fetch();
+echo $testDept->getDeptAbbr();
+$testDept->setDeptId("2");
+$testDept->fetch();
+echo $testDept->getDeptAbbr();
+
 
 function printExample($resultsArr){
 	foreach($resultsArr as $arr){
--- database/department.php
+++ database/department.php
@@ -1,12 +1,77 @@
 <?php
-private $servername = "facultydb.cdh6zybsklle.us-east-1.rds.amazonaws.com"
-private $username = "test"
-private $password = "test"
-private $dbname = "facultydb"
 
-$database = new Database();
+class department{
 
-public function insert($deptID. $deptName, $deptAbbr){
+private $deptId = "";
+private $deptName = "";
+private $deptAbbr = "";
+private $conn;
+
+
+
+public function getDeptAbbr(){
+	return $this->deptAbbr;
+}
+
+public function setDeptId($id){
+	$this->deptId = $id;
+}
+
+
+	function __construct($conn,$deptId){
+		$this->conn = $conn;
+		$this->deptId = $deptId;
+	}
+	
+	
+public function fetch(){
+	$resultsArr = $this->conn->getData("select * from department where departmentID = :id",array(
+		":id" => $this->deptId
+		)
+	);
+	try{
+		$results = $resultsArr[0];
+		$this->deptName = $results['departmentName'];
+		$this->deptAbbr = $results['departmentAbbr'];
+		return true;
+	}
+	catch(Exception $e){
+		return false;
+	}
+	
+}
+public function fetch($whereStmt,$input){
+	$resultsArr = $this->conn->getData("select * from department where".$whereStmt,$input);
+	try{
+		$results = $resultsArr[0];
+		$this->deptName = $results['departmentName'];
+		$this->deptAbbr = $results['departmentAbbr'];
+		return true;
+	}
+	catch(Exception $e){
+		return false;
+	}
+}
+
+
+public function put(){
+	//update
+}
+
+
+
+public function post(){
+	//insert
+}
+
+public function delete(){
+	//delete
+}
+
+
+
+
+/*public function insert($deptID. $deptName, $deptAbbr){
 	$database->query('INSERT INTO Department (departmentID, departmentName, departmentAbbr) values ($deptID, $deptName, $deptAbbr)');
 	$database->bind('$deptID', 999);
 	$database->bind('$deptName', 'Dept');
@@ -24,4 +89,7 @@ public function update($deptID. $deptName, $deptAbbr){
 
 	$database->execute();
 }
->
+
+*/
+}
+?>
