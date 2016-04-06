858509c5a6a8911d1b11bbe9b57bac67308a649e
adistasi <add5980@rit.edu>
2016-04-06 11:47:37 -0400

    Created search.php (provides functionality for live search)
    search.php is the target page for the AJAX call to accommodate for live search on the addProfessor.php page

---
+++ database/search.php
@@ -0,0 +1,14 @@
+<?php
+	require("data.php");
+	require("employees.php");
+
+	$name = $_POST['name'];
+
+	$database = new data;
+
+	$results = $database->getData("SELECT fName, lName, roomNumber, facultyId FROM Employees WHERE fName LIKE '%" . $name . "%' OR lName LIKE '%" . $name . "%';", array());
+
+	foreach($results as $arr) {
+		echo "<li onclick='setActive(this);'><span class='fId' style='display: none'>" . $arr['facultyId'] . "</span><strong>" . $arr['lName'] . ", " . $arr['fName'] . "</strong><br /><span class='rmNum initialism'>" . $arr['roomNumber'] . "</span><hr /></li>";
+	}
+?>
