0e1e763ab4cded391eaa59e38ea41d2bfc5c0efd
adistasi <add5980@rit.edu>
2016-04-23 02:14:36 -0400

    Added addslashes() to info passed into live search for admin & employee.

--- database/adminSearch.php
+++ database/adminSearch.php
@@ -4,10 +4,11 @@
 	require("filters.php");
 
 	$name = filterString($_POST['name']);
+	$cleanName = addslashes($name);
 
 	$database = new data;
 
-	$results = $database->getData("SELECT fName, lName, departmentAbbr, adminId FROM Admin JOIN department ON Admin.departmentId = department.departmentId WHERE fName LIKE '%" . $name . "%' OR lName LIKE '%" . $name . "%' OR CONCAT(fName, ' ', lName) LIKE '%" . $name . "%' ORDER BY lname ASC;", array());
+	$results = $database->getData("SELECT fName, lName, departmentAbbr, adminId FROM Admin JOIN department ON Admin.departmentId = department.departmentId WHERE fName LIKE '%" . $cleanName . "%' OR lName LIKE '%" . $cleanName . "%' OR CONCAT(fName, ' ', lName) LIKE '%" . $cleanName . "%' ORDER BY lname ASC;", array());
 
 	foreach($results as $arr) {
 		echo "<li onclick='setAdminActive(this); disableCreate();'><span class='aId' style='display: none'>" . $arr['adminId'] . "</span><strong>" . $arr['lName'] . ", " . $arr['fName'] . "</strong><br /><span class='rmNum initialism'>" . $arr['departmentAbbr'] . "</span><hr /></li>";
--- database/search.php
+++ database/search.php
@@ -4,10 +4,11 @@
 	require("filters.php");
 
 	$name = filterString($_POST['name']);
+	$cleanName = addslashes($name);
 
 	$database = new data;
 
-	$results = $database->getData("SELECT fName, lName, roomNumber, facultyId FROM Employees WHERE fName LIKE '%" . $name . "%' OR lName LIKE '%" . $name . "%' OR CONCAT(fName, ' ', lName) LIKE '%" . $name . "%' ORDER BY lname ASC;", array());
+	$results = $database->getData("SELECT fName, lName, roomNumber, facultyId FROM Employees WHERE fName LIKE '%" . $cleanName . "%' OR lName LIKE '%" . $cleanName . "%' OR CONCAT(fName, ' ', lName) LIKE '%" . $cleanName . "%' ORDER BY lname ASC;", array());
 
 	foreach($results as $arr) {
 		echo "<li onclick='setActive(this);'><span class='fId' style='display: none'>" . $arr['facultyId'] . "</span><strong>" . $arr['lName'] . ", " . $arr['fName'] . "</strong><br /><span class='rmNum initialism'>" . $arr['roomNumber'] . "</span><hr /></li>";
