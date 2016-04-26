1c41bc57ad9c59be55c207978e5c3f896061ae0a
maxwellstorm <mxs5194@rit.edu>
2016-04-26 17:32:06 -0400

    added cinfigurtion file for database credentials

--- database/configuration.php
+++ database/configuration.php
@@ -2,5 +2,5 @@
 define (DB_USER, "maxwellsweikert");
 define (DB_PASSWORD, "M3312140m");
 define (DB_DATABASE, "facultyDb");
-define (DB_HOST, "facultydb.cdh6zybsklle.us-east-1.rds.amazonaws.com'");
+define (DB_HOST, "facultydb.cdh6zybsklle.us-east-1.rds.amazonaws.com");
 ?>
