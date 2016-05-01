dadc94d244354ce53ae16daa5493a9e9bf12fa88
Student <Student>
2016-05-01 15:57:36 -0400

    added db info

---
+++ database/dbInfo.php
@@ -0,0 +1,10 @@
+<?php
+
+class dbInfo{
+	public $DBSERVER = "facultydb.cdh6zybsklle.us-east-1.rds.amazonaws.com";
+	public $DBUSER = "maxwellsweikert";
+	public $DBPASSWORD = "M3312140m";
+	public $DBNAME = "facultyDb";
+	
+}
+?>
